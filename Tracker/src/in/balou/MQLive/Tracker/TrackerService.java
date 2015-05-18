a638f929fb6c922ed42d2437cc2664242c6412de
machgo <marco.ms.schmid@gmail.com>
2015-05-18 20:48:05 +0200

    added location-stuff

--- Tracker/src/in/balou/MQLive/Tracker/TrackerService.java
+++ Tracker/src/in/balou/MQLive/Tracker/TrackerService.java
@@ -1,7 +1,9 @@
 package in.balou.MQLive.Tracker;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.IBinder;
 
 /**
@@ -12,6 +14,11 @@ public class TrackerService extends Service
     @Override
     public IBinder onBind(Intent intent)
     {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        MyLocationListener listener = new MyLocationListener();
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 4000, 0, listener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 4000, 0, listener);
         return null;
     }
 }
