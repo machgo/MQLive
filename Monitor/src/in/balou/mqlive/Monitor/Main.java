a0f8d7876c42f4ddb016cf9a505cb53de23e2565
machgo <marco.ms.schmid@gmail.com>
2015-06-08 07:45:50 +0200

    changed window-title

--- Monitor/src/in/balou/mqlive/Monitor/Main.java
+++ Monitor/src/in/balou/mqlive/Monitor/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("MQLive Monitor");
         primaryStage.setScene(new Scene(root, 761, 750));
         primaryStage.show();
 
